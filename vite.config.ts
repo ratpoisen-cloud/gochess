4069dd31749f7361dd9749e844b0cea0545e77ba
Rat Poisen <ratpoisen@Rats-MacBook-Air.local>
2026-05-25 22:28:41 +0300

    chore: dynamic VITE_BASE in vite.config, .env.*.local in gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gochess_alternative/',
+  base: process.env.VITE_BASE || '/gochess_alternative/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
